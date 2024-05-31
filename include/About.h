d2eb5e2039d4d20616ce6b26b6dd7560b7fb4b10
GrKoR <dr.g.kornev@gmail.com>
2024-06-01 00:03:49 +0300

    fix: board_v1 url, board_ver++

--- include/About.h
+++ include/About.h
@@ -5,7 +5,7 @@ namespace About
 	static constexpr char name[] = "ButtonsECU";
 	static constexpr char desc[] = "Buttons control board for Pixel project";
 	static constexpr char board_type = Consts::BOARD_TYPE_BUTTONS1;		// 5 bits
-	static constexpr char board_ver = 1;		// 3 bits
+	static constexpr char board_ver = 2;		// 3 bits
 	static constexpr char soft_ver = 1;			// 6 bits
 	static constexpr char can_ver = 1;			// 2 bits
 	static constexpr char git[] = "https://github.com/starfactorypixel/SlaveECU_Buttons";
