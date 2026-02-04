f3dceb6fd848df8c898c4daa12541ee8e1b9643f
Dragon-Knight <dubki4132@mail.ru>
2026-02-04 20:22:36 +0300

    Change branch to v2.1

--- include/About.h
+++ include/About.h
@@ -5,8 +5,8 @@ namespace About
 	static constexpr char name[] = "ButtonsECU";
 	static constexpr char desc[] = "Buttons control board for Pixel project";
 	static constexpr char board_type = Consts::BOARD_TYPE_BUTTONS1;		// 5 bits
-	static constexpr char board_ver = 2;		// 3 bits
-	static constexpr char soft_ver = 2;			// 6 bits
+	static constexpr char board_ver = 3;		// 3 bits
+	static constexpr char soft_ver = 3;			// 6 bits
 	static constexpr char can_ver = 1;			// 2 bits
 	static constexpr char git[] = "https://github.com/starfactorypixel/SlaveECU_Buttons";
 	
