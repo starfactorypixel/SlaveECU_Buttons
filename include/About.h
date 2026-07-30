bc79c9a0595bb9f6c8100c0166d28eb64653f927
Dragon_Knight <dubki4132@mail.ru>
2026-07-30 19:09:33 +0300

    fix `board_type` const

--- include/About.h
+++ include/About.h
@@ -1,10 +1,11 @@
 #pragma once
+#include <ConstantLibrary.h>
 
 namespace About
 {
 	static constexpr char name[] = "ButtonsECU";
 	static constexpr char desc[] = "Buttons control board for Pixel project";
-	static constexpr char board_type = Consts::BOARD_TYPE_BUTTONS1;		// 5 bits
+	static constexpr char board_type = Consts::BOARD_TYPE_BUTTONS;		// 5 bits
 	static constexpr char board_ver = 3;		// 3 bits
 	static constexpr char soft_ver = 3;			// 6 bits
 	static constexpr char can_ver = 1;			// 2 bits
